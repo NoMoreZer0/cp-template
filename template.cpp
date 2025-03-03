6f7b9bb85cf091c3d73bafa5ad7e4faf32f32604
Adi Abilgaziyev <kazakupersuper@mail.ru>
2025-03-03 09:16:34 +0500

    add template

---
+++ template.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+
+#ifndef ONLINE_JUDGE
+#include "debug.h"
+#else
+#define debug(...)
+#define debugArr(...)
+#endif
+
+#define pb push_back
+#define sz(x) (int)x.size()
+#define whole(x) x.begin(), x.end()
+#define rwhole(x) x.rbegin(), x.rend()
+
+#define se second
+#define fi first
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+
+const int N = (int)2e5+2;
+const int INF = 1e9 + 7;
+const int mod = (int)1e9 + 7;
+const ll linf = (ll)1e15 + 1;
+
+
+void solve() {
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int tt = 1; //cin >> tt;
+    while (tt--) solve();    
+    return 0;
+}
+ 
